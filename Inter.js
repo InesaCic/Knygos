8d080e2ebc45acc93f7a7410d504db292e5cc73d
InesaCic <42993488+InesaCic@users.noreply.github.com>
2018-09-20 21:20:28 +0300

    Button says WOW

--- Inter.js
+++ Inter.js
@@ -1,5 +1,7 @@
 
- 
+ var button = document.getElementById("interactive-button");
+ button.addEventListener("click", function(){ alert("Wow") } , false);
+    
     $(document).ready(function(){
         $("#interactive-checkbox").on("click", function(){
             alert("Wow");
