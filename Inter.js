cb985bac752f1f5eac78fc5d902ff6e13e83b6e0
InesaCic <42993488+InesaCic@users.noreply.github.com>
2018-09-30 22:34:24 +0300

    Atnaujinimas

--- Inter.js
+++ Inter.js
@@ -1,10 +1,18 @@
 
- var button = document.getElementById("interactive-button");
- button.addEventListener("click", function(){ alert("Wow") } , false);
-    
-    $(document).ready(function(){
-        $("#interactive-checkbox").on("click", function(){
-            alert("Wow");
-        });
+ var button = document.getElementById('interactive-button');
+ {
+ button.addEventListener('click', function(){ alert("Wow") } , false);
+ }
+ 
+$(document).ready(function(){
+    $("#interactive-checkbox").on("click", function(){
+        alert("Wow");
     });
-
+});
+    $('.button').on('click', function(){
+        var email = $('#email').val();
+        /* THIS CODE WILL BE RUN EVERY TIME ON BUTTON CLICK */
+    });
+    
+    
+    $('.success').show();
